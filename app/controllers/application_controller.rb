e8dae1497ab8c89cc56021778494ef6e3cc0f924
ebkn <ktennis.mqekr12@gmail.com>
2018-04-30 15:51:55 +0900

    feat: setup glaphql

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  include Clearance::Controller
+
+  skip_before_action :verify_authenticity_token
+
+  protected
+
+  def authenticate_by_api_token!
+    sign_in User.first
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module GraphqlRailsTest
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.autoload_paths << Rails.root.join('app', 'graph', 'types')
   end
 end
